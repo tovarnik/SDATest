54c35c2351644ea6735062c99d82eec52a7d3fd5
tovarnik <standa.jonak@gmail.com>
2022-05-11 10:03:46 +0200

    Update Unreachable.py

--- Unreachable.py
+++ Unreachable.py
@@ -4,5 +4,5 @@ This will likely fail due to print function after return
 
 def calculate_sum(x, y):
        	return x + y
-print("After return...!")
+print("After return...!"
 print('Sum is', calculate_sum(1,4))
